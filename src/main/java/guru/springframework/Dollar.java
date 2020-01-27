ff3ccd4ef9ad328ae8c2902ad41c5b7ea0616b43
pawel-bialas <zylex@tlen.pl>
2020-01-28 00:01:30 +0100

    value objects testing

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -1,6 +1,32 @@
 package guru.springframework;
 
+import java.util.Objects;
+
 public class Dollar {
 
 
+     int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+
+    }
+
+    Dollar times (int multiplier) {
+
+        return new Dollar(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dollar dollar = (Dollar) o;
+        return amount == dollar.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
 }
